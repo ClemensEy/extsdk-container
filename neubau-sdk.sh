a4b96fdad67c735455b2ee444fca39f19bc21904
Clemens Schmiegel <clemens.schmiegel@gmail.com>
2019-01-25 14:45:37 +0100

    ssh server added

--- neubau-sdk.sh
+++ neubau-sdk.sh
@@ -5,8 +5,14 @@
 # make sure Xquarz is installed and configured to support X forwarding via network
 # eg http://server.neubau.io:50080/x86_64-buildtools-nativesdk-standalone-2.4.sh
 url=$1
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-echo "Neubau sdk-script..."
+echo "Neubau sdk-script"
+echo "mount xtools toolchain"
+hdiutil attach $SCRIPTPATH/xtools-2016-09-01.dmg &
 #shared directory (see https://github.com/crops/docker-win-mac-docs/wiki/Mac-Instructions)
 sambarun=$(docker inspect -f {{.State.Running}} samba)
 #echo $sambarun
@@ -17,11 +23,14 @@ else
   echo "starting samba container"
   docker start samba && sudo ifconfig lo0 127.0.0.2 alias up
 fi
-#open socat in background (for X forwarding)
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 
 #get ip
-myip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep -Fv 127.0.0.2 | awk '{print $2}')
+#myip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep -Fv 127.0.0.2 | awk '{print $2}')
+myip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep -Fv 127.0.0.2 | awk 'NR>0 && NR<2 {print $2}')
+
+#open socat in background (for X forwarding)
+socat TCP-LISTEN:6000,bind=${myip},reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+
 
 
 #run docker with our without url (http://server.neubau.io:50080/sdk-installer.sh)
